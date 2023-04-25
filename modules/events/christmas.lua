87ee34506121d0be973270f0e333aee59c08ad58
m00nyONE <severin.meier.x@gmail.com>
2023-04-25 19:23:38 +0200

    add arcanist Christmas class icon

--- modules/events/christmas.lua
+++ modules/events/christmas.lua
@@ -8,7 +8,8 @@ local christmasIcons = {
     [3] = "HodorReflexes/assets/events/christmas/class_nightblade_christmas.dds",
     [4] = "HodorReflexes/assets/events/christmas/class_warden_christmas.dds",
     [5] = "HodorReflexes/assets/events/christmas/class_necromancer_christmas.dds",
-    [6] = "HodorReflexes/assets/events/christmas/class_templar_christmas.dds"
+    [6] = "HodorReflexes/assets/events/christmas/class_templar_christmas.dds",
+    [7] = "HodorReflexes/assets/events/christmas/class_arcanist_christmas.dds"
 }
 
 local function Initialize()
