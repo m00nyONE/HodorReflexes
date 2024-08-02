064b89ee8c7e87f29bab3ce8c261868c8a7f7bb3
B7TxSpeed <speed56400@gmail.com>
2024-08-03 00:09:06 +0200

    :sparkles: Add corsair icons

---
+++ users/corsair/corsair.lua
@@ -0,0 +1,23 @@
+local u = HodorReflexes.users
+local a = HodorReflexes.anim.users
+
+-- Corsair rosters
+u["@Alcadeios"] = {"Alca", "|cffe34eA|r|cffcb40l|r|cffb233c|r|cff9925a|r", "HodorReflexes/users/corsair/alcadeios.dds"}
+u["@B7TxSpeed"] = {"Speed", "|c00fffeSpeed|r", "HodorReflexes/users/corsair/b7txspeed.dds"}
+u["@Beynh"] = {"Beynh", "|cffef6bBeynh|r", "HodorReflexes/users/corsair/beynh.dds"}
+u["@DiabloESO"] = {"DiabloESO", "|cffaa01D|r|cff9502i|r|cff7f03a|r|cff6a04b|r|cff5504l|r|cff4005o|r|cff2a06E|r|cff1507S|r|cff0008O|r", "HodorReflexes/users/corsair/DiabloESO_3.dds"}
+u["@Diaphora666"] = {"Diaphora", "|cfeb992D|r|cfeb5a1i|r|cfdb0b1a|r|cfdacc1p|r|cfca7d0h|r|cfba2e0o|r|cfb9eefr|r|cfa99ffa|r", "HodorReflexes/users/corsair/diaphora.dds"}
+u["@Dinokunslaad"] = {"Dinokunslaad", "|cb58721D|r|cad7b20i|r|ca6701fn|r|c9e641eo|r|c96581dk|r|c8e4c1cu|r|c86401an|r|c7e3419s|r|c772918l|r|c6f1d17a|r|c671116a|r|c5f0515d|r", "HodorReflexes/users/corsair/dinokunslaad.dds"}
+u["@Feiito"] = {"Feiito", "|ca32bfdF|r|c8b55fbe|r|c7380f9i|r|c5aaaf7i|r|c42d4f5t|r|c2afff3o|r", "HodorReflexes/users/corsair/feiito.dds"}
+u["@Gabnich"] = {"Gab", "|cf44f88G|r|cfa7591a|r|cff9b99b|r", "HodorReflexes/users/corsair/Gabnich.dds"}
+u["@Ipnoz"] = {"Ipnoz", "|cff2117I|r|cff4315p|r|cff6412n|r|cff8610o|r|cffa70ez|r", "HodorReflexes/users/corsair/ipnoz.dds"}
+u["@Reachy"] = {"Reachy", "|cfffb00R|r|cfff800e|r|cfff500a|r|cfff200c|r|cffef00h|r|cffec00y|r", "HodorReflexes/users/corsair/reachy.dds"}
+u["@Thelinor"] = {"Thelinor", "|cc1c2c0T|r|cafafaeh|r|c9c9d9ce|r|c8a8b8al|r|c787877i|r|c666665n|r|c535353o|r|c414141r|r", "HodorReflexes/users/corsair/thelinor.dds"}
+u["@xTazu"] = {"xTazu", "|cd6931bx|r|caeae1aT|r|c85c918a|r|c5de417z|r|c34ff15u|r", "HodorReflexes/users/corsair/xtazu.dds"}
+
+-- Friends
+u["@AppelleMoiPepette"] = {"Pepette", "|cffae4aP|r|cffbc68e|r|cffc986p|r|cffd7a5e|r|cffe4c3t|r|cfff2e1t|r|cffffffe|r", "HodorReflexes/users/corsair/pepette.dds"}
+u["@Halox48"] = {"Haløx ", "|c00e6ffH|r|c00cfffa|r|c00b9ffl|r|c00a2ffø|r|c008cffx|r ", "HodorReflexes/users/corsair/Halox2.dds"}
+a["@Halox48"] = {"HodorReflexes/users/corsair/Halox2_anim.dds", 48, 1, 10}
+u["@JohnAmok"] = {"Oli", "|c1143ffO|r|c2169ffl|r|c328effi|r", "HodorReflexes/users/corsair/oli.dds"}
+u["@Lulu_Laventus"] = {"Lulu Laventus", "|c1e08ffL|r|c2408ffu|r|c2b07ffl|r|c3106ffu|r |c3705ffL|r|c3d05ffa|r|c4304ffv|r|c4903ffe|r|c5002ffn|r|c5602fft|r|c5c01ffu|r|c6200ffs|r", "HodorReflexes/users/corsair/lulu.dds"}
