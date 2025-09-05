6bdc375b85dca1d47aaa7c5d454e4b5f198e6445
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-09-05 13:00:35 +0200

    rewrite util extension

--- extensions/util.lua
+++ extensions/util.lua
@@ -1,19 +1,26 @@
-HodorReflexes.util = {}
+local addon_name = "HodorReflexes"
+local addon = _G[addon_name]
 
-local HR = HodorReflexes
-local util = HR.util
+local extension = {
+    name = "util",
+    version = "1.0.0",
+}
+local extension_name = extension.name
+local extension_version = extension.version
+
+addon[extension_name] = extension
 
 local strformat = string.format
 
 -- Returns map distance, not meters
-function util.GetDistance(x1, y1, x2, y2)
+function extension.GetDistance(x1, y1, x2, y2)
 
 	return zo_sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 
 end
 
 -- Convert FFFFFF to 1, 1, 1
-function util.Hex2RGB(hex)
+function extension.Hex2RGB(hex)
 
     hex = hex:gsub("#", "")
     return tonumber("0x" .. hex:sub(1, 2)) / 255, tonumber("0x" .. hex:sub(3, 4)) / 255, tonumber("0x" .. hex:sub(5, 6)) / 255
@@ -21,20 +28,20 @@ function util.Hex2RGB(hex)
 end
 
 -- Convert 1, 1, 1 to FFFFFF
-function util.RGB2Hex(r, g, b)
+function extension.RGB2Hex(r, g, b)
 
 	return strformat("%.2x%.2x%.2x", zo_round(r * 255), zo_round(g * 255), zo_round(b * 255))
 
 end
 
 -- color code string
-function util.ColorCode(hex, str)
+function extension.ColorCode(hex, str)
 	return "|c" .. hex .. str .. "|r"
 end
 
 -- Remove duplicate values from a table.
 -- Only works for simple tables like: 1=>'a', 2=>'b', 3=>'b'
-function util.TableUnique(t)
+function extension.TableUnique(t)
 
 	local hash = {}
 	local res = {}
@@ -48,12 +55,12 @@ function util.TableUnique(t)
 
 end
 
-function util.IsValidString(s)
+function extension.IsValidString(s)
 	return type(s) == 'string' and s ~= ''
 end
 
 
-function util.spairs(t, sortFunction) -- thanks @Solinur <3
+function extension.spairs(t, sortFunction) -- thanks @Solinur <3
 
     -- collect the keys
     local keys = {}
