8c068a7b69e2b7501089d7380e5a4cc8be0c4af2
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-05 17:42:01 +0200

    add console missing libs screen

--- core/Console/LibCheck.lua
+++ core/Console/LibCheck.lua
@@ -10,4 +10,39 @@ local logger = core.logger.main
 local LCN = LibCustomNames
 
 -- no need to check for LCN or LCI here, as those do not exist on Console
-function core.OptionalLibrariesCheck() end
+function core.OptionalLibrariesCheck()
+    local sw = core.sw
+    local dialogName = string.format("%s_MISSING_LIBS", addon_name)
+
+    if (not LCN) and not sw.libraryPopupDisabled then
+        logger:Warn("LibCustomNames is missing. Some features will be disabled.")
+        ZO_Dialogs_RegisterCustomDialog(dialogName, {
+            title = {
+                text = GetString(HR_MISSING_LIBS_TITLE_CONSOLE),
+            },
+            mainText = {
+                text = GetString(HR_MISSING_LIBS_TEXT_CONSOLE),
+            },
+            buttons = {
+                {
+                    text = GetString(HR_MISSING_LIBS_OK),
+                    keybind = "DIALOG_PRIMARY",
+                    callback = function() end,
+                },
+                {
+                    text = GetString(HR_MISSING_LIBS_DONTSHOWAGAIN),
+                    keybind = "DIALOG_RESET",
+                    callback = function()
+                        sw.libraryPopupDisabled = true
+                    end,
+                },
+            },
+            mustChoose = true,
+            canQueue = true,
+            allowShowOnDead = false,
+        }, nil, IsInGamepadPreferredMode())
+
+        ZO_Dialogs_ShowDialog(dialogName)
+    end
+
+end
