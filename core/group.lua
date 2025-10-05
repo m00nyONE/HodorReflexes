9af2200390d2e8b31bd9efaaf629925afc8cdb21
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-05 18:51:03 +0200

    add group structure

---
+++ core/group.lua
@@ -0,0 +1,10 @@
+-- SPDX-FileCopyrightText: 2025 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
+local addon_name = "HodorReflexes2"
+local addon = _G[addon_name]
+local internal = addon.internal
+local core = internal.core
+local logger = core.logger.main
+
+local CM = core.CM
