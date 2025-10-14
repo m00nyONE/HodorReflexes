b7544c83865c4dc9d981fb9c6140461be9e82d64
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-14 16:32:43 +0200

    adjust dps module sv's to how it will be working later

--- modules/dps/main.lua
+++ modules/dps/main.lua
@@ -36,11 +36,6 @@ local moduleDefinition = {
         accountWide = true,
         disableInPvP = true,
 
-        damageListEnabled = 0, -- 0=off, 1=always, 2=out of combat, 3=non bossfights
-        damageListWidth = 227,
-        damageListPosLeft = 0,
-        damageListPosTop = 50,
-
         damageListHeaderOpacity = 0.8,
         damageListRowEvenOpacity = 0.65,
         damageListRowOddOpacity = 0.45,
@@ -49,10 +44,16 @@ local moduleDefinition = {
         colorDamageTotal = "faffb2", -- light yellow
         colorDamageBoss = "b2ffb2", -- light green
     },
+    --svDefaultDamageList = {
+    --    damageListEnabled = 0, -- 0=off, 1=always, 2=out of combat, 3=non bossfights
+    --    damageListWidth = 227,
+    --    damageListPosLeft = 0,
+    --    damageListPosTop = 50,
+    --},
 
     isTestRunning = false,
 
-    --damageList = internal.ListClass:New(addon_name .. "dps" .. "_DamageList"),
+    --damageList = internal.ListClass:New("dps"),
 }
 
 local module = internal.moduleClass:New(moduleDefinition)
