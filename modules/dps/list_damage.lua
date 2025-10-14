628f2d2faf3d04686e018021857b5e7471934ba7
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-15 00:32:00 +0200

    create Basic minimal dps list

--- modules/dps/list_damage.lua
+++ modules/dps/list_damage.lua
@@ -12,6 +12,9 @@ local internal_modules = internal.modules
 local module_name = "dps"
 local module = addon_modules[module_name]
 
+local LGCS = LibGroupCombatStats
+local DAMAGE_UNKNOWN = LGCS.DAMAGE_UNKNOWN
+
 local svDefault = {
     enabled =  1, -- 1=always, 2=out of combat, 3=non bossfights, 0=off
     disableInPvP = true,
@@ -29,8 +32,70 @@ function module:CreateDamageList()
         Update = function() self:UpdateDamageList() end,
     }
     self.damageList = internal.listClass:New(listDefinition)
+
+    self:RunOnce("SetupDamageList")
+end
+
+function module:SetupDamageList()
+
+    local function createDamageRowCreationWrapper(wrappedFunction)
+        return function(rowControl, data, scrollList)
+            -- only create rows if conditions are met
+            if data.dmg > 0 and (self.isTestRunning or IsUnitOnline(data.tag)) then
+                wrappedFunction(self, rowControl, data, scrollList)
+            end
+        end
+    end
+
+    ZO_ScrollList_AddDataType(
+            self.damageList.listControl,
+            1,
+            "HodorReflexes_Dps_DamageList_DamageRow",
+            self.damageList.sv.listRowHeight,
+            createDamageRowCreationWrapper(self.damageRowCreationFunction)
+    )
+end
+
+function module:damageRowCreationFunction(rowControl, data, scrollList)
+    local userId = data.userId
+    local userName = userId
+    local classId = data.classId
+    local defaultIcon = GetClassIcon(classId)
+
+    local nameControl = rowControl:GetNamedChild('_Name')
+    nameControl:SetText(userName)
+    nameControl:SetColor(1, 1, 1)
+    local iconControl = rowControl:GetNamedChild('_Icon')
+    iconControl:SetTextureCoords(0, 1, 0, 1)
+    iconControl:SetTexture(defaultIcon)
+
+    local valueControl = rowControl:GetNamedChild("_Value")
+    valueControl:SetText(self:getDamageRowFormat(data.dmgType, data.dmg, data.dps))
+    valueControl:SetFont("$(GAMEPAD_MEDIUM_FONT)|$(KB_19)|outline")
 end
 
 function module:UpdateDamageList()
-    d(self.name)
+    local listControl = self.damageList.listControl
+
+    local dmgType = DAMAGE_UNKNOWN
+
+    ZO_ScrollList_Clear(listControl)
+    local dataList = ZO_ScrollList_GetDataList(listControl)
+
+    local playersDataList = {}
+    for _, playerData in pairs(addon.playersData) do
+        if playerData.dmg > 0 then
+            dmgType = playerData.dmgType
+            table.insert(playersDataList, playerData)
+        end
+    end
+    table.sort(playersDataList, function(...) self:sortByDamageType(...) end)
+
+    -- insert damageRows
+    for i, playerData in ipairs(playersDataList) do
+        playerData.orderIndex = i
+        table.insert(dataList, ZO_ScrollList_CreateDataEntry(1, playerData))
+    end
+
+    ZO_ScrollList_Commit(listControl)
 end
