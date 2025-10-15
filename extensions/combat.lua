cb495ef896ec8b253db97a4f8a98d1a1ff974293
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-15 11:41:33 +0200

    allow calculation of damage over time

--- extensions/combat.lua
+++ extensions/combat.lua
@@ -18,9 +18,18 @@ local extensionDefinition = {
     version = "1.0.0",
     svDefault = {},
 
+    damageHistory = {
+        {
+            timeStamp = 0,
+            damageDone = 0,
+        },
+    },
+
     data = {
         dpstime = 0,
         hpstime = 0,
+        groupDPSOut = 0,
+        damageOutTotalGroup = 0,
     },
 }
 
@@ -34,14 +43,61 @@ function extension:Activate()
     --addon.RegisterCallback(HR_EVENT_COMBAT_END, function() self:Reset() end)
 end
 
-function extension:GetCombatTime()
-    return zo_roundToNearest(zo_max(self.data.dpstime, self.data.hpstime), 0.1)
-end
 function extension:Reset()
     self.data.dpstime = 0
     self.data.hpstime = 0
+    self.data.groupDPSOut = 0
+    self.data.damageOutTotalGroup = 0
+    self.damageHistory = {
+        {
+            timeStamp = 0,
+            damageDone = 0,
+        },
+    }
 end
 function extension:FightRecapCallback(_, data)
     self.data.dpstime = data.dpstime
     self.data.hpstime = data.hpstime
+    self.data.groupDPSOut = data.groupDPSOut
+    self.data.damageOutTotalGroup = data.damageOutTotalGroup
+
+    -- save history
+    table.insert(self.data.damageHistory, {
+        timestamp = GetGameTimeMilliseconds(),
+        damage = data.damageOutTotalGroup
+    })
+end
+
+function extension:GetCombatTime()
+    return zo_roundToNearest(zo_max(self.data.dpstime, self.data.hpstime), 0.1)
+end
+function extension:GetGroupDPSOut()
+    return self.data.groupDPSOut
+end
+function extension:GetDamageOutTotalGroup()
+    return self.data.damageOutTotalGroup
+end
+function extension:GetGroupDPSOverTime(seconds)
+    local now = GetGameTimeMilliseconds()
+    local cutoff = now - seconds * 1000
+    local oldest, newest
+
+    -- Finde ältesten und neuesten Wert im Zeitraum
+    for i = #self.data.damageHistory, 1, -1 do
+        local entry = self.data.damageHistory[i]
+        if not newest and entry.timestamp <= now then
+            newest = entry
+        end
+        if entry.timestamp <= cutoff then
+            oldest = entry
+            break
+        end
+    end
+
+    if oldest and newest and newest.timestamp > oldest.timestamp then
+        local damageDiff = newest.damage - oldest.damage
+        local timeDiff = newest.timestamp - oldest.timestamp
+        return damageDiff / timeDiff
+    end
+    return 0
 end
