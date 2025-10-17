eb3738c1cd415c3880b4ae4df4cfbc67fb929298
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-17 09:35:57 +0200

    set default update interval

--- modules/dps/util.lua
+++ modules/dps/util.lua
@@ -109,7 +109,7 @@ function module:CreateFightTimeUpdaterOnControl(list, control)
     end
     control._onCombatStart = function()
         control._onCombatStop()
-        EM:RegisterForUpdate(list._eventId .. "TimerUpdate", list.sw.timerUpdateInterval, renderFightTimeToControl)
+        EM:RegisterForUpdate(list._eventId .. "TimerUpdate", list.sw.timerUpdateInterval or 100, renderFightTimeToControl)
     end
     -- register timer update callbacks
     addon.RegisterCallback(HR_EVENT_COMBAT_START, control._onCombatStart)
