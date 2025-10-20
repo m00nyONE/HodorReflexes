d3eec4dc63163c5eed7ef01dfea4296527d06163
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-20 19:44:44 +0200

    add minimal menu

--- core/PC/menu.lua
+++ core/PC/menu.lua
@@ -7,8 +7,80 @@ local internal = addon.internal
 local core = internal.core
 local logger = core.initSubLogger("menu")
 
+local util = addon.util
+
+local LAM = LibAddonMenu2
+
+function core.GetExtensionOptions()
+    local options = {
+        {
+            type = "header",
+            name = string.format("|cFFFACD%s|r", "Extensions")
+        }
+    }
+    for extensionName, extension in util.Spairs(addon.extensions, util.SortByPriority) do
+        table.insert(options, {
+            type = "checkbox",
+            name = extension.friendlyName or extensionName,
+            tooltip = extension.description or "",
+            default = true,
+            getFunc = function() return core.sw.extensions[extensionName] end,
+            setFunc = function(value)
+                core.sw.extensions[extensionName] = value
+            end,
+            requiresReload = true,
+        })
+    end
+
+    return options
+end
+
+function core.GetModuleOptions()
+    local options = {
+        {
+            type = "header",
+            name = string.format("|cFFFACD%s|r", "Modules")
+        }
+    }
+    for moduleName, module in util.Spairs(addon.modules, util.SortByPriority) do
+        table.insert(options, {
+            type = "checkbox",
+            name = module.friendlyName or moduleName,
+            tooltip = module.description or "",
+            default = true,
+            getFunc = function() return core.sw.modules[moduleName] end,
+            setFunc = function(value)
+                core.sw.modules[moduleName] = value
+            end,
+            requiresReload = true,
+        })
+    end
+
+    return options
+end
+
 --- build the menu for PC platform
 --- @return void
 function core.BuildMenu()
     logger:Info("Building menu for PC")
+    local panel = {
+        type = "panel",
+        name = addon.friendlyName,
+        displayName = string.format('|cFFFACD%s|r', addon.friendlyName),
+        author = addon.author,
+        version = addon.version,
+        website = "https://www.esoui.com/downloads/info2311-HodorReflexes-DPSULTtracker.html#donate",
+        donation = addon.Donate,
+        registerForRefresh = true,
+    }
+    local options = {}
+    for _, opt in ipairs(core.GetExtensionOptions()) do
+        table.insert(options, opt)
+    end
+    for _, opt in ipairs(core.GetModuleOptions()) do
+        table.insert(options, opt)
+    end
+
+    LAM:RegisterAddonPanel(addon_name .. "_Menu", panel)
+    LAM:RegisterOptionControls(addon_name .. "_Menu", options)
 end
