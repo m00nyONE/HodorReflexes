cad2ac4bc6ed6f08646725511e658f32a8cbaa9d
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-21 10:02:31 +0200

    update docs

--- core/modules.lua
+++ core/modules.lua
@@ -9,16 +9,15 @@ local logger = core.initSubLogger("modules")
 
 local util = addon.util
 
-
---- initializes all registered modules if they are enabled in the saved variables.
---- creates saved variables for each module.
---- registers LibGroupBroadcast protocols if available.
---- registers mainMenu options if available.
---- registers submenu if available.
---- runs the module's initialization function.
+--- Initializes all registered modules if they are enabled in the saved variables.
+--- Creates saved variables for each module.
+--- Registers LibGroupBroadcast protocols if available.
+--- Registers main menu options if available.
+--- Registers submenu options if available.
+--- Runs the module's activation function.
 --- @return void
 function core.InitializeModules()
-    -- check if saved variables are populated
+    -- Check if saved variables are populated
     if not core.sw.modules then
         logger:Warn("No modules found in saved variables, populating with defaults...")
         core.sw.modules = core.svDefault.modules
@@ -33,78 +32,87 @@ function core.InitializeModules()
         end
         if isEnabled then
             logger:Info("Initializing module: %s", moduleName)
-            module:RunOnce("CreateSavedVariables") -- create saved variables for the module
-            module:RunOnce("RegisterLGBProtocols", core.LGBHandler) -- register LibGroupBroadcast Protocols if available
+            module:RunOnce("CreateSavedVariables") -- Create saved variables for the module
+            module:RunOnce("RegisterLGBProtocols", core.LGBHandler) -- Register LibGroupBroadcast protocols if available
 
-            -- get menu options if available
+            -- Get main menu options if available
             local mainMenuOptions = module:RunOnce("GetMainMenuOptions")
             if mainMenuOptions then
                 core.RegisterMainMenuOptions(module.friendlyName, mainMenuOptions)
             end
 
+            -- Get submenu options if available
             local subMenuOptions = module:RunOnce("GetSubMenuOptions")
             if subMenuOptions then
                 core.RegisterSubMenuOptions(module.friendlyName, subMenuOptions)
             end
 
-            module:RunOnce("Activate") -- run the module's initialization function
+            module:RunOnce("Activate") -- Run the module's activation function
             module.enabled = true
         end
     end
 end
 
---- registers a module to the core
---- @param module moduleClass the module to register
+--- Registers a module to the core.
+--- @param module moduleClass The module to register.
 --- @return void
 function core.RegisterModule(module)
     if addon.modules[module.name] then
         logger:Error("Module " .. module.name .. " is already registered!")
         return
     end
-    module.enabled = false -- set enabled to false by default
-    addon.modules[module.name] = module -- add module to the addon.modules table
+    module.enabled = false -- Set enabled to false by default
+    addon.modules[module.name] = module -- Add module to the addon.modules table
     logger:Debug("Successfully registered module: " .. module.name)
 end
 
-
---- @class: moduleClass
+---@class moduleClass : ZO_InitializingObject
 local module = ZO_InitializingObject:Subclass()
 internal.moduleClass = module
 
--- must implement functions
-module:MUST_IMPLEMENT("Activate") -- function that gets called to activate the module when it's enabled
+-- Must implement functions
+module:MUST_IMPLEMENT("Activate") -- Function that gets called to activate the module when it's enabled
 
--- can implement functions
+-- Optional functions
+--- Registers LibGroupBroadcast protocols if available.
+--- @param handler any The handler for protocol registration.
+--- @return nil
 function module:RegisterLGBProtocols(handler)
     self.RegisterLGBProtocols = nil
     return nil
 end
 
+--- Returns submenu options if available.
+--- @return nil
 function module:GetSubMenuOptions()
     self.GetSubMenuOptions = nil
     return nil
 end
 
+--- Returns main menu options if available.
+--- @return nil
 function module:GetMainMenuOptions()
     self.GetMainMenuOptions = nil
     return nil
 end
 
+--- Returns diagnostic data for the module if available.
+--- @return nil
 function module:GetDiagnostic()
     self.GetDiagnosticInfo = nil
     return nil
 end
 
---- returns whether the module is enabled
---- @return boolean true if the module is enabled, false otherwise
+--- Returns whether the module is enabled.
+--- @return boolean true if the module is enabled, false otherwise.
 function module:IsEnabled()
     return self.enabled
 end
 
---- runs a function only once and then removes it from the object
---- @param funcName string the name of the function to run
---- @param ... any the arguments to pass to the function
---- @return any the return value of the function, or nil if the function does not exist
+--- Runs a function only once and then removes it from the object.
+--- @param funcName string The name of the function to run.
+--- @param ... any Arguments to pass to the function.
+--- @return any The return value of the function, or nil if the function does not exist.
 function module:RunOnce(funcName, ...)
     if type(self[funcName]) == "function" then
         local result = self[funcName](self, ...)
@@ -114,8 +122,9 @@ function module:RunOnce(funcName, ...)
     return nil
 end
 
---- initializes the moduleClass
---- @param t table a table containing the properties to set on the moduleClass
+--- Initializes the moduleClass.
+--- Sets the properties of the module based on the given definition.
+--- @param moduleDefinition table A table containing the properties of the module.
 --- @return void
 function module:Initialize(moduleDefinition)
     assert(type(moduleDefinition) == "table", "moduleDefinition must be a table")
@@ -130,19 +139,20 @@ function module:Initialize(moduleDefinition)
         self[k] = v
     end
 
-    -- create a new subLogger for the module
+    -- Create a new subLogger for the module
     self.logger = core.initSubLogger(self.name)
 
-    -- register the module to the core
+    -- Register the module to the core
     core.RegisterModule(self)
 end
 
---- creates the saved variables for the module
+--- Creates the saved variables for the module.
+--- Uses a combination of account-wide and per-character saved variables.
 --- @return void
 function module:CreateSavedVariables()
     local svNamespace = string.format("module_%s", self.name)
     local svVersion = core.svVersion + self.svVersion
-    -- we use a combination of accountWide saved variables and per character saved variables. This little swappi swappi allows us to switch between them without defining new variables
+    -- Use a combination of account-wide and per-character saved variables.
     self.sw = ZO_SavedVars:NewAccountWide(core.svName, svVersion, svNamespace, self.svDefault)
     if not core.sw.accountWide then
         self.sv = ZO_SavedVars:NewCharacterIdSettings(core.svName, svVersion, svNamespace, self.svDefault)
