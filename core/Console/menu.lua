672a517f9788f594ad2b24faa607ed03442498ba
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-23 13:53:31 +0200

    create menu for console

--- core/Console/menu.lua
+++ core/Console/menu.lua
@@ -7,8 +7,82 @@ local internal = addon.internal
 local core = internal.core
 local logger = core.initSubLogger("menu")
 
---- build the menu for Console platform
---- @return void
-function core.BuildMenu()
-    logger:Info("Building menu for Console")
+local LHAS = LibHarvensAddonSettings
+
+-- function is platform specific
+function core.GetPanelConfig(subName)
+    local name = addon.friendlyName
+    if subName then
+        name = string.format("%s - %s", addon.friendlyName, subName)
+    end
+
+    return LHAS:AddAddon(name, {
+        allowDefaults = true,
+        allowRefresh = true,
+    })
+end
+
+-- function is platform specific
+function core.CreateSectionHeader(name)
+    return {
+        type = LHAS.ST_SECTION,
+        label = string.format("|cFFFACD%s|r", name),
+    }
+end
+
+-- function is platform specific
+function core.CreateNewMenu(subName, options)
+    local panel = core.GetPanelConfig(subName)
+    for _, option in ipairs(options) do
+        LHAS:AddSetting(panel, option)
+    end
+end
+
+-- function is platform specific
+function core.GetCoreMenuOptions()
+    local options = core.CreateSectionHeader("General")
+    local general = {
+        {
+            type = LHAS.ST_CHECKBOX,
+            label = "account wide settings",
+            tooltip = "enable/disable account-wide settings.",
+            default = true,
+            getFunc = function() return core.sw.accountWide end,
+            setFunc = function(value) core.sw.accountWide = value end,
+            default = true,
+        },
+        -- we do not need ui lock/unlock on console. They have no mouse anyway :D
+    }
+    for _, option in ipairs(general) do
+        table.insert(options, option)
+    end
+
+    table.insert(options, core.CreateSectionHeader("Modules"))
+    for moduleName, module in util.Spairs(addon.modules, util.SortByPriority) do
+        table.insert(options, {
+            type = LHAS.ST_CHECKBOX,
+            label = module.friendlyName or moduleName,
+            tooltip = module.description or "",
+            default = true,
+            getFunc = function() return core.sw.modules[moduleName] end,
+            setFunc = function(value)
+                core.sw.modules[moduleName] = value
+            end,
+        })
+    end
+    table.insert(options, core.CreateSectionHeader("Extensions"))
+    for extensionName, extension in util.Spairs(addon.extensions, util.SortByPriority) do
+        table.insert(options, {
+            type = LHAS.ST_CHECKBOX,
+            label = extension.friendlyName or extensionName,
+            tooltip = extension.description or "",
+            default = true,
+            getFunc = function() return core.sw.extensions[extensionName] end,
+            setFunc = function(value)
+                core.sw.extensions[extensionName] = value
+            end,
+        })
+    end
+
+    return options
 end
