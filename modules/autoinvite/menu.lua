09f376e1a2503bf2453d3162b4e63a461a09fdfe
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-23 09:43:13 +0200

    create basic autoinvite skeleton

---
+++ modules/autoinvite/menu.lua
@@ -0,0 +1,17 @@
+-- SPDX-FileCopyrightText: 2025 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
+local addon_name = "HodorReflexes"
+local addon = _G[addon_name]
+local internal = addon.internal
+local core = internal.core
+
+local addon_modules = addon.modules
+local internal_modules = internal.modules
+
+local module_name = "autoinvite"
+local module = addon_modules[module_name]
+
+function module:GetSubMenuOptions()
+    return nil
+end
