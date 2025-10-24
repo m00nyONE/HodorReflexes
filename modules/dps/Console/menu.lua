a3cfddcd29cc1492b95e0851e0a55b0d5d9a5c67
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-24 09:43:00 +0200

    add console DPS menu

--- modules/dps/Console/menu.lua
+++ modules/dps/Console/menu.lua
@@ -6,14 +6,212 @@ local addon = _G[addon_name]
 local internal = addon.internal
 local core = internal.core
 
+local util = addon.util
+local LHAS = LibHarvensAddonSettings
+
 local addon_modules = addon.modules
 local internal_modules = internal.modules
 
 local module_name = "dps"
 local module = addon_modules[module_name]
 
+-- TODO: translations
 function module:GetSubMenuOptions()
-    return {
+    local function mergeOptions(source, destination)
+        for _, option in ipairs(source) do
+            if not option.isAdvancedSetting or self.sw.advancedSettings then
+                table.insert(destination, option)
+            end
+        end
+    end
 
+    local function GetGeneralOptions()
+        return {
+            core.CreateSectionHeader("General"),
+            {
+                type = LHAS.ST_CHECKBOX,
+                label = "account wide settings",
+                tooltip = "enable/disable account-wide settings.",
+                default = true,
+                getFunction = function() return self.sw.accountWide end,
+                setFunction = function(value)
+                    self.sw.accountWide = value
+                end,
+                requiresReload = true,
+            },
+            {
+                type = LHAS.ST_CHECKBOX,
+                label = "Advanced Settings",
+                tooltip = "allows you to customize more advanced settings for the lists.",
+                default = false,
+                getFunction = function() return self.sw.advancedSettings end,
+                setFunction = function(value)
+                    self.sw.advancedSettings = value
+                end,
+                requiresReload = true,
+            },
+        }
+    end
+    local function GetComonListOptions(listName, list)
+        return {
+            core.CreateSectionHeader(listName),
+            {
+                type = LHAS.ST_CHECKBOX,
+                label = "Disable in PvP",
+                tooltip = "disable the list when in PvP.",
+                default = list.svDefault.disableInPvP,
+                getFunction = function() return list.sw.disableInPvP end,
+                setFunction = function(value)
+                    list.sw.disableInPvP = value
+                    list:RefreshVisibility()
+                end,
+            },
+            {
+                type = LHAS.ST_DROPDOWN,
+                label = "Visibility",
+                tooltip = "set the visibility of the list.",
+                default = GetString(HR_VISIBILITY_SHOW_ALWAYS),
+                items = {
+                    {name = GetString(HR_VISIBILITY_SHOW_NEVER), data = 0},
+                    {name = GetString(HR_VISIBILITY_SHOW_ALWAYS), data = 1},
+                    {name = GetString(HR_VISIBILITY_SHOW_OUT_OF_COMBAT), data = 2},
+                    {name = GetString(HR_VISIBILITY_SHOW_NON_BOSSFIGHTS), data = 3},
+                },
+                getFunction = function() return list.sw.enabled end,
+                setFunction = function(value)
+                    list.sw.enabled = value
+                    list:RefreshVisibility()
+                end,
+                width = "full",
+            },
+            {
+                type = LHAS.ST_SLIDER,
+                label = "List width",
+                min = list.svDefault.windowWidth,
+                max = list.svDefault.windowWidth + 150,
+                step = 1,
+                format = "%.0f",
+                unit = " px",
+                default = list.svDefault.windowWidth,
+                getFunction = function() return list.sw.windowWidth end,
+                setFunction = function(value)
+                    list.sw.windowWidth = value
+                    list.window:SetWidth(list.sw.windowWidth)
+                end,
+                isAdvancedSetting = true,
+            },
+        }
+    end
+
+
+    local options = GetGeneralOptions()
+    local damageList = GetComonListOptions("Damage List", self.damageList)
+    local damageListSpecificOptions = {
+        {
+            type = LHAS.ST_COLOR,
+            label = "Total Damage Color",
+            tooltip = "",
+            default = util.Hex2RGB(self.damageList.svDefault.colorDamageTotal),
+            getFunction = function() return util.Hex2RGB(self.damageList.sw.colorDamageTotal) end,
+            setFunction = function(r, g, b)
+                self.damageList.sw.colorDamageTotal = util.RGB2Hex(r, g, b)
+                self.damageList:Update()
+            end,
+            isAdvancedSetting = true,
+        },
+        {
+            type = LHAS.ST_COLOR,
+            label = "Boss Damage Color",
+            tooltip = "",
+            default = util.Hex2RGB(self.damageList.svDefault.colorDamageBoss),
+            getFunction = function() return util.Hex2RGB(self.damageList.sw.colorDamageBoss) end,
+            setFunction = function(r, g, b)
+                self.damageList.sw.colorDamageBoss = util.RGB2Hex(r, g, b)
+                self.damageList:Update()
+            end,
+            isAdvancedSetting = true,
+        },
+        {
+            type = LHAS.ST_COLOR,
+            label = "player row highlight color",
+            tooltip = "highlight color for the player's row in the damage list.",
+            default = unpack(self.damageList.svDefault.listPlayerHighlightColor),
+            getFunction = function() return unpack(self.damageList.sw.listPlayerHighlightColor) end,
+            setFunction = function(r, g, b, o)
+                self.damageList.sw.listPlayerHighlightColor = {r, g, b, o}
+                self.damageList:Update()
+            end,
+            isAdvancedSetting = true,
+        },
+        {
+            type = LHAS.ST_SLIDER,
+            label = "Header Opacity",
+            tooltip = "set the opacity of the damage list header.",
+            min = 0,
+            max = 1,
+            step = 0.05,
+            format = "%.2f",
+            default = self.damageList.svDefault.listHeaderOpacity,
+            getFunction = function() return self.damageList.sw.listHeaderOpacity end,
+            setFunction = function(value)
+                self.damageList.sw.listHeaderOpacity = value
+                self.damageList:Update()
+            end,
+            isAdvancedSetting = true,
+        },
+        {
+            type = LHAS.ST_SLIDER,
+            label = "Even Row Opacity",
+            tooltip = "set the opacity of the even rows in the damage list.",
+            min = 0,
+            max = 1,
+            step = 0.05,
+            format = "%.2f",
+            default = self.damageList.svDefault.listRowEvenOpacity,
+            getFunction = function() return self.damageList.sw.listRowEvenOpacity end,
+            setFunction = function(value)
+                self.damageList.sw.listRowEvenOpacity = value
+                self.damageList:Update()
+            end,
+            isAdvancedSetting = true,
+        },
+        {
+            type = LHAS.ST_SLIDER,
+            label = "Odd Row Opacity",
+            tooltip = "set the opacity of the odd rows in the damage list.",
+            min = 0,
+            max = 1,
+            step = 0.05,
+            format = "%.2f",
+            default = self.damageList.svDefault.listRowOddOpacity,
+            getFunction = function() return self.damageList.sw.listRowOddOpacity end,
+            setFunction = function(value)
+                self.damageList.sw.listRowOddOpacity = value
+                self.damageList:Update()
+            end,
+            isAdvancedSetting = true,
+        },
+        {
+            type = LHAS.ST_SLIDER,
+            label = "Timer Interval (ms)",
+            tooltip = "set the update interval of timers in the damage list (in milliseconds). Lower values increase update frequency but may impact performance.",
+            min = 50,
+            max = 1000,
+            step = 25,
+            format = "%.0f",
+            unit = " ms",
+            default = self.damageList.svDefault.timerUpdateInterval,
+            getFunction = function() return self.damageList.sw.timerUpdateInterval end,
+            setFunction = function(value)
+                self.damageList.sw.timerUpdateInterval = value
+            end,
+            requiresReload = true,
+            isAdvancedSetting = true,
+        },
     }
+
+    mergeOptions(damageListSpecificOptions, damageList)
+    mergeOptions(damageList, options)
+
+    return options
 end
