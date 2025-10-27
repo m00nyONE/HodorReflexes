4bc1650bc38a8b46443a3cf80c708f2ebdd9d245
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-27 17:06:07 +0100

    finish hideMe module implementation

--- modules/hideme/main.lua
+++ modules/hideme/main.lua
@@ -13,6 +13,9 @@ local group = internal.group
 
 local localPlayer = "player"
 
+local HR_EVENT_PLAYER_ACTIVATED = addon.HR_EVENT_PLAYER_ACTIVATED
+local HR_EVENT_GROUP_CHANGED = addon.HR_EVENT_GROUP_CHANGED
+
 local moduleDefinition = {
     name = "hideme",
     friendlyName = "Hide Me",
@@ -22,25 +25,54 @@ local moduleDefinition = {
     enabled = false,
     svVersion = 1,
     svDefault = {
-        hideDamage = false,
-        hideHorn = false,
-        hideColos = false,
-        hideAtro = false,
-        hideSaxhleel = false,
+        preferences = {}, -- will look something like this: [1] = true, [2] = false, ...
     },
+
+    updateDebounceInterval = 1500, -- ms
+
+    hideIds = {
+        [1] = {
+            name = "hideDamage",
+            label = "Hide Damage",
+            description = "Hide your damage numbers from other group members dps list.",
+        },
+        [2] = {
+            name = "hideHorn",
+            label = "Hide Horn",
+            description = "Hide your horn from from horn and compact list.",
+        },
+        [3] = {
+            name = "hideColos",
+            label = "Hide Colossus",
+            description = "Hide your colossus from colos & compact list.",
+        },
+        [4] = {
+            name = "hideAtro",
+            label = "Hide Atro",
+            description = "Hide your atronach from atro and compact list.",
+        },
+        [5] = {
+            name = "hideSaxhleel",
+            label = "Hide Saxhleel",
+            description = "Hide your saxhleel from horn and compact list.",
+        },
+    }
 }
 
 local module = internal.moduleClass:New(moduleDefinition)
 
 function module:Activate()
-    self.logger:Debug("activated Hide Me module")
+    -- generate data fields
+    local fields = {}
+    for _, hide in ipairs(self.hideIds) do
+        fields[hide.name] = false
+    end
+    group.RegisterPlayersDataFields(fields)
 
-    group.RegisterPlayersDataFields({
-        hideDamage = false,
-        hideHorn = false,
-        hideColos = false,
-        hideAtro = false,
-        hideSaxhleel = false,
-    })
+    self:RunOnce("RegisterEvents")
 end
 
+function module:RegisterEvents()
+    addon.RegisterCallback(HR_EVENT_PLAYER_ACTIVATED, function(...) self:SendHideMeMessageDebounced(true) end)
+    addon.RegisterCallback(HR_EVENT_GROUP_CHANGED, function(...) self:SendHideMeMessageDebounced() end)
+end
--- modules/hideme/protocol.lua
+++ modules/hideme/protocol.lua
@@ -17,25 +17,29 @@ local module = addon_modules[module_name]
 local LGB = LibGroupBroadcast
 local localPlayer = "player"
 
+local EM = GetEventManager()
+
 local protocolHideMe = {}
 local MESSAGE_ID_HIDEME = 32
 
 function module:RegisterLGBProtocols(handler)
+    local CreateFlagField = LGB.CreateFlagField
     local CreateNumericField = LGB.CreateNumericField
     local CreateArrayField = LGB.CreateArrayField
     local protocolOptions = {
         isRelevantInCombat = false
     }
 
+    -- Each hideField must have a unique ID. That way the protocol does not break when new fields are added later and group members still have not updated yet.
+    -- Its enough to send only the IDs of the fields that are supposed to be hidden. everything else will be considered visible.
     protocolHideMe = handler:DeclareProtocol(MESSAGE_ID_HIDEME, "Hide Me")
-    -- create array field. each hideField must have a unique ID. That way the protocol does not break when new fields are added later and group members still have not updated yet.
-    -- its enough to send only the IDs of the fields that are supposed to be hidden. everything else will be considered visible.
-    -- Example:
-    -- protocolHideMe:AddField(CreateArrayField("hide", CreateNumericField("id", {
-    --     minValue = 1,
-    --     maxValue = 15,
-    -- })))
-    --protocolHideMe:AddField()
+    protocolHideMe:AddField(CreateFlagField("syncRequest", {
+        defaultValue = false,
+    }))
+    protocolHideMe:AddField(CreateArrayField("hideIds", CreateNumericField("id", {
+        minValue = 1,
+        maxValue = 15,
+    })))
     protocolHideMe:OnData(function(...) self:onHideMeMessageReceived(...) end)
     protocolHideMe:Finalize(protocolOptions)
 end
@@ -43,38 +47,41 @@ end
 function module:onHideMeMessageReceived(unitTag, data)
     if AreUnitsEqual(unitTag, localPlayer) then return end -- don't receive own messages
 
-    local hideDamage = false
-    local hideHorn = false
-    local hideColos = false
-    local hideAtro = false
-    local hideSaxhleel = false
-
-    for _, id in ipairs(data.showHorn) do
-        if id == 1 then hideDamage = true
-        elseif id == 2 then hideHorn = true
-        elseif id == 3 then hideColos = true
-        elseif id == 4 then hideAtro = true
-        elseif id == 5 then hideSaxhleel = true
+    if data.syncRequest then
+        self:SendHideMeMessageDebounced()
     end
 
-
-        module.logger:Debug("Received hide me message from %s: hide field ID %d", GetUnitName(unitTag), id)
-    end
-
-    group.CreateOrUpdatePlayerData({
+    local playerData = {
         name     = GetUnitName(unitTag),
         tag      = unitTag,
-        hideDamage = hideDamage,
-        hideHorn = hideHorn,
-        hideColos = hideColos,
-        hideAtro = hideAtro,
-        hideSaxhleel = hideSaxhleel,
-    })
+    }
 
+    for _, id in ipairs(data.hideIds) do
+        local hideOptionName = self.hideIds[id.id] and self.hideIds[id.id].name
+        if hideOptionName then
+            playerData[hideOptionName] = true
+        end
+    end
+
+    group.CreateOrUpdatePlayerData(playerData)
 end
 
-function module:SendHideMeMessage()
+function module:SendHideMeMessage(syncRequest)
+    local data = {}
+    for id, enabled in pairs(self.sv.preferences) do -- intentionally use pairs instead of ipairs to avoid sending empty IDs
+        if enabled then
+            table.insert(data, {id = id})
+        end
+    end
     protocolHideMe:Send({
-        -- preferences data
+        syncRequest = syncRequest or false,
+        hideIds = data
     })
+end
+
+function module:SendHideMeMessageDebounced(syncRequest)
+    EM:RegisterForUpdate(addon_name .. "_HidemeMessageDebounce", self.updateDebounceInterval, function()
+        EM:UnregisterForUpdate(addon_name .. "_HidemeMessageDebounce")
+        self:SendHideMeMessage(syncRequest)
+    end)
 end
