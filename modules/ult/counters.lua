23276f88791ea44e3da4596e3c147fb90a69c314
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-10-28 11:48:35 +0100

    add horn counter

---
+++ modules/ult/counters.lua
@@ -0,0 +1,70 @@
+-- SPDX-FileCopyrightText: 2025 m00nyONE
+-- SPDX-License-Identifier: Artistic-2.0
+
+local addon_name = "HodorReflexes"
+local addon = _G[addon_name]
+local internal = addon.internal
+local core = internal.core
+
+local addon_modules = addon.modules
+local addon_extensions = addon.extensions
+local internal_modules = internal.modules
+
+local module_name = "ult"
+local module = addon_modules[module_name]
+
+local counterClass = internal.counterClass
+local util = addon.util
+
+local localPlayer = "player"
+
+local HR_EVENT_PLAYER_DATA_UPDATED = addon.HR_EVENT_PLAYER_DATA_UPDATED
+local HR_EVENT_GROUP_CHANGED = addon.HR_EVENT_GROUP_CHANGED
+
+function module:CreateHornCounter()
+    local function updateFunc()
+        local count = 0
+        local ready = true
+        for i = 1, GetGroupSize() do
+            local unitTag = GetGroupUnitTagByIndex(i)
+            if util.IsUnitInPlayersRange(unitTag, 20) then
+                count = count + 1
+            elseif GetGroupMemberSelectedRole(unitTag) == LFG_ROLE_DPS then
+                ready = false
+            end
+        end
+
+        return count, ready
+    end
+
+    local def = {
+        name = "horn",
+        texture = "esoui/art/icons/ability_ava_003_a.dds",
+        distance = 20,
+        updateInterval = 100,
+        svDefault = {
+            accountWide = false,
+            enabled = 1, -- 1=always, 2=only in combat, 0=off
+            windowPosLeft = 400,
+            windowPosTop = 200,
+            scale = 1.0,
+        },
+        updateFunc = updateFunc,
+    }
+    self.hornCounter = counterClass:New(def)
+
+    addon.RegisterCallback(HR_EVENT_PLAYER_DATA_UPDATED, function(playerData)
+        if not playerData.isPlayer then return end
+
+        if playerData.hasHorn or playerData.hasSaxhleel then
+            self.hornCounter:SetActive(true)
+        else
+            self.hornCounter:SetActive(false)
+        end
+    end)
+    addon.RegisterCallback(HR_EVENT_GROUP_CHANGED, function()
+        if not IsUnitGrouped(localPlayer) then
+            self.hornCounter:SetActive(false)
+        end
+    end)
+end
