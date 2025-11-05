50945d569df7db4085c2baa2f95651da51fffe42
m00nyONE <23294566+m00nyONE@users.noreply.github.com>
2025-11-05 17:16:41 +0100

    fix memoryleaks when creating playerData

--- modules/hideme/protocol.lua
+++ modules/hideme/protocol.lua
@@ -47,6 +47,7 @@ function module:RegisterLGBProtocols(handler)
     protocolHideMe:Finalize(protocolOptions)
 end
 
+local playerDataCache = {}
 function module:onHideMeMessageReceived(unitTag, data)
     if data.syncRequest and not AreUnitsEqual(unitTag, localPlayer) then
         self.logger:Debug("Received HideMe sync request from %s, sending current preferences.", GetUnitName(unitTag))
@@ -54,23 +55,22 @@ function module:onHideMeMessageReceived(unitTag, data)
     end
     self.logger:Debug("Received HideMe message from %s with IDs: %s", GetUnitName(unitTag), table.concat(data.ids, ", "))
 
-    local playerData = {
-        name     = GetUnitName(unitTag),
-        tag      = unitTag,
-    }
+    ZO_ClearTable(playerDataCache) -- we need to clear it here because the table is variable. Even tho we later set everything to false, it can be that the client has an older version than the sender and thus receiving additional data it cannot process
+    playerDataCache.name = GetUnitName(unitTag)
+    playerDataCache.tag = unitTag
 
-    for id, hideId in pairs(self.hideIds) do
-        playerData[hideId.name] = false
+    for _, hideId in pairs(self.hideIds) do
+        playerDataCache[hideId.name] = false
     end
 
     for _, id in pairs(data.ids) do
         local hideOptionName = self.hideIds[id] and self.hideIds[id].name
         if hideOptionName then
-            playerData[hideOptionName] = true
+            playerDataCache[hideOptionName] = true
         end
     end
 
-    group.CreateOrUpdatePlayerData(playerData)
+    group.CreateOrUpdatePlayerData(playerDataCache)
 end
 
 local hideProtocolCache = {}
